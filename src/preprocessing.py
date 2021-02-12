63360194ff6fe3cfc23ceb673fd8fe4b0ce429ac
jotwo <jokotek@gmail.com>
2021-02-12 09:26:05 +0100

    las modifs for basic extractor

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,26 +4,19 @@ import string
 from io import StringIO
 
 import pandas as pd
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+import spacy
 from pdfminer.high_level import extract_text
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 class PDFCorpusPreprocessor:
-    def __init__(self, reports_dir_path='../data/reports'):
-        nltk.download('wordnet', quiet=True)
-        nltk.download('punkt', quiet=True)
+    def __init__(self, reports_dir_path):
         self.reports_dir_path = reports_dir_path
         self.pdf_report_names = [
             r for r in os.listdir(self.reports_dir_path) if r.endswith('.pdf')
         ]
         self.corpus = None
         self.doc_term_matrix = None
-        self.i = 0
-
-
 
     def extract_corpus_from_pdf(self):
         temp_corpus = {}
@@ -39,15 +32,19 @@ class PDFCorpusPreprocessor:
     def build_document_term_matrix(self, lemmatize=True):
         if self.corpus is not None:
 
+            if lemmatize:
+                corpus_to_clean = pd.DataFrame(
+                    self.corpus['content'].apply(self.__lemmatize_text)
+                )
+            else:
+                corpus_to_clean = self.corpus
+
+
             cleaned_corpus = pd.DataFrame(
-                self.corpus['content'].apply(self.__clean_report_content)
+                corpus_to_clean['content'].apply(self.__clean_report_content)
             )
 
-            # if lemmatize:
-            #     cleaned_corpus = cleaned_corpus['content'].apply(self.__lemmatize_text)
-
             vectorizer = CountVectorizer(stop_words='english')
-            # vectorizer = CountVectorizer(stop_words='english', tokenizer=word_tokenize)
             count_vectorized_data = vectorizer.fit_transform(cleaned_corpus['content'])
 
             self.doc_term_matrix = pd.DataFrame(
@@ -56,33 +53,32 @@ class PDFCorpusPreprocessor:
                 index=self.corpus.index,
             )
 
-            return self.doc_term_matrix
+        else:
+            raise Exception(
+                "You first have to extract a corpus to build a document-term matrix."
+            )
+    
+    def get_document_term_matrix(self):
+        if self.doc_term_matrix is not None:
+            return self.doc_term_matrix.copy()
 
         else:
             raise Exception(
-                "You first have to extract a corpus to build a document-term matrix"
+                "You first have to extract a corpus and build a document-term matrix before demanding to get one."
             )
     
-    def __lemmatize_text(self, text):
-        """Transform each token of the text to its lemma individually and returns it."""
-        lemmatizer = WordNetLemmatizer()
-        if self.i == 0:
-            print(text)
-            print('\n')
-        token_list = nltk.word_tokenize(text)
-        if self.i == 0:
-            print(token_list[2])
-            print('\n')
-        text = ' '.join([lemmatizer.lemmatize(t) for t in token_list])
-        if self.i == 0:
-            # print(text)
-            # print('\n')
-            print(lemmatizer.lemmatize(token_list[2]))
-        self.i = self.i+1
-        return text
+    def get_corpus(self):
+        if self.corpus is not None:
+            return self.corpus.copy()
+
+        else:
+            raise Exception(
+                "You first have to extract a corpus before demanding to get one."
+            )
 
     def __clean_report_content(self, text):
         """Make text lowercase, remove text in square brackets, remove punctuation, remove words containing numbers and other unwanted substrings."""
+        
         url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
         email_regex = "[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}"
         # remove URLs
@@ -104,6 +100,13 @@ class PDFCorpusPreprocessor:
         # remove some remaining parts of math expressions
         text = re.sub("ˆ[A-Za-z]*", "", text)
 
-        text = self.__lemmatize_text(text)
+        return text
+    
+    def __lemmatize_text(self, text):
+        """Transform each token of the text to its lemma individually and returns it."""
+
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(text)
+        text = ' '.join([token.lemma_ for token in doc])
 
         return text
