5bb0645867beda15f02ffc656d75e7d93059fe0a
Manasa Devi Noolu <70909929+manasanoolu7@users.noreply.github.com>
2021-02-17 21:17:40 +0100

    Update app.py

--- humain-nlp-project-jo/src/app.py
+++ humain-nlp-project-jo/src/app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, session, send_file
 from werkzeug.utils import secure_filename
 import re
+
 import pandas as pd
 import spacy
 from spacy import displacy
@@ -28,15 +29,8 @@ if not os.path.isdir(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'csv', 'json'])
-
-"""
-def parse_json_file(file):
-    col_names=['function','industry']
-    csv_data=pd.read_json(file,names=col_names,header=None)
-    for i,row in csv_data.iterrows():
-        print(i,row['function'],row['industry'])
-def parse_csv_file(file):
-"""
+# seting the maximum column width to display all columns
+pd.set_option('display.max_colwidth', -1)
 
 
 def allowed_file(filename):
@@ -67,55 +61,90 @@ def upload_file():
             if file and allowed_file(file.filename):
 
                 filename = secure_filename(file.filename)
-
+                global detailed_df
+                global paragraphs_df
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                pdf_corpus.add_pdf(os.path.join(UPLOAD_FOLDER, filename))
+
+                docs_df = pdf_corpus.get_docs_df().copy()
+
+                paragraphs_df = pdf_corpus.get_paragraphs_df().copy()
+                sentences_df = pdf_corpus.get_sentences_df().copy()
+                tokens_df = pdf_corpus.get_tokens_df().copy()
+
+                detailed_df = tokens_df[['token']].copy()
+                detailed_df['doc_name'] = tokens_df['doc_id'].apply(lambda x: docs_df.loc[x, 'name'])
+                detailed_df['paragraph'] = tokens_df['paragraph_id'].apply(lambda x: paragraphs_df.loc[x, 'paragraph'])
+                detailed_df['sentence'] = tokens_df['sentence_id'].apply(lambda x: sentences_df.loc[x, 'sentence'])
+
+                usecase_df = pd.read_csv('E:/BeCodeProjects/HumAIn_Project/src/most_likely_usecase_per_paragraph.csv')
+                detailed_df['function'] = usecase_df['function']
+                detailed_df['industry'] = usecase_df['industry']  # .apply(lambda x: usecase_df.loc[x, 'industry'])
+                detailed_df['usecase'] = usecase_df['usecase']  # .apply(lambda x: usecase_df.loc[x, 'usecase'])
+                detailed_df = detailed_df[['doc_name', 'paragraph', 'sentence', 'function', 'industry', 'usecase']]
+
+                # print(detailed_df.columns)
+
                 flash('File(s) successfully uploaded')
 
+
             else:
+
                 flash('Upload file(s) in pdf format')
 
         return redirect('/')
 
-@app.route('/text')
+
+@app.route('/text', methods=['POST'])
 def text():
-    return render_template('text_extractor.html')
+    if request.method == 'POST':
+        global detailed_df
+        global paragraphs_df
+        global text
+        text = paragraphs_df['paragraph']
+
+        return render_template('text_extractor.html', text=text)
+
+
 @app.route('/process', methods=['POST'])
 def text_processing():
     if request.method == 'POST':
-        # with open('E:/BeCodeProjects/HumAIn_Project/text0.txt','r',encoding="utf8") as f:
-        # return render_template("text_extractor.html",text=f.read())
-        choice = request.form.get('taskoption')
+        global detailed_df
+        global text,results,num_of_results
 
-        rawtext = request.form.get('rawtext')
+        choice1 = request.form.get('taskoption')  # Function
+        choice2 = request.form.get('taskoption2')  # Industry
+        # print(detailed_df)
 
-        print(rawtext)
-        doc = nlp(rawtext)
-        #print(doc)
-        d = []
-        for sent in doc.sents:
-            #print(ent)
-            d.append((sent.label_, sent.text))
-            df = pd.DataFrame(d, columns=('industry', 'output'))
-            print(df)
-            test = df['output']
-            print(test)
+        exhibit_ind = detailed_df.loc[detailed_df['industry'] == 'exhibit']['usecase']
+        exhibit_fn = detailed_df.loc[detailed_df['function'] == 'exhibit']['usecase']
+        dr_ind = detailed_df.loc[detailed_df['industry'] == 'data richness']['usecase']
+        dr_fn = detailed_df.loc[detailed_df['function'] == 'data richness']['usecase']
+        pa_ind = detailed_df.loc[detailed_df['industry'] == 'predictive analytics']['usecase']
+        pa_fn = detailed_df.loc[detailed_df['function'] == 'productivity and growth']['usecase']
+        if choice1 == 'exhibit' or choice2 == 'exhibit':
+            results = exhibit_ind
+            text = detailed_df['paragraph']
 
-        if choice == 'Marketing':
-            results = test
-            print(results)
             num_of_results = len(results)
-        """    
-        elif choice == 'Sales':
-            results = PERSON_named_entity
+        if choice1 == 'data richness' or choice2 == 'data richness':
+            results = dr_ind
+            text = detailed_df['paragraph']
             num_of_results = len(results)
-        elif choice == 'IT':
-            results = GPE_named_entity
+        if choice1 == 'predictive analytics' or choice2 == 'productivity and growth':
+            results = pa_ind
+            text = detailed_df['paragraph']
             num_of_results = len(results)
-        elif choice == 'Data':
-            results = MONEY_named_entity
-            num_of_results = len(results)
-        """
+
     return render_template("text_extractor.html", results=results, num_of_results=num_of_results)
+
+
+@app.route('/files')
+def display_files():
+    global detailed_df
+    global text,results,num_of_results
+    text = text
+    return render_template('text_extractor.html', results=results, num_of_results=num_of_results,text=text)
 
 
 if __name__ == '__main__':
