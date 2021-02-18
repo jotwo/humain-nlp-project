49e15805b6426a860e275dc25ca986db840b9ef5
phfimmers <philippefimmers@gmail.com>
2021-02-18 18:41:11 +0100

    add QnA model download

--- app/src/download_model.py
+++ app/src/download_model.py
@@ -1,7 +1,10 @@
-"""This file defines the function for downloading our finetuned BERT model"""
+"""This file defines the function for downloading our finetuned BERT
+classifier model and the pretrained Bert QnA model"""
+
 import requests
 import sys
 from pathlib import Path
+from transformers import BertForQuestionAnswering
 
 
 def download_model():
@@ -18,8 +21,22 @@ def download_model():
                 if chunk:
                     modelfile.write(chunk)
                     chunk_progress += 1
-                    print(f"Downloaded: {chunk_progress*8}MB\n")
+                    print(f"Downloading model 1/2 in background: {chunk_progress*8}MB")
                     sys.stdout.flush()
             else:
                 open("model_downloaded", "w").close()
+    
+    if (
+        not Path("modelqna_downloaded").is_file()
+        or not Path("./BertLSquad/pytorch_model.bin").is_file()
+    ):
+        print(f"Started model 2/2 download in background")
+        sys.stdout.flush()
+        model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+        model.save_pretrained("./BertLSquad")
+        open("modelqna_downloaded", "w").close()
+        print("Model 2/2 download completed")
+        sys.stdout.flush()
+
     return
+
